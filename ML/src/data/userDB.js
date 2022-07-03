6a8a875e3047bbaedd0981135a3ffa123099927a
alejandrosoares <soaresalejandro@outlook.com>
2022-07-02 21:48:26 -0300

    feat: add user data close #2

---
+++ ML/src/data/userDB.js
@@ -0,0 +1,20 @@
+[
+    {
+        "id": 1,
+        "name": "Alejandro",
+        "surname": "Soares",
+        "email": "ale.soares@gmail.com",
+        "password": "ale123",
+        "role": "user",
+        "avatar": ""
+    },
+    {
+        "id": 2,
+        "name": "Juana",
+        "surname": "lopez",
+        "email": "juana.lopez@gmail.com",
+        "password": "ale123",
+        "role": "user",
+        "avatar": ""
+    }
+]
